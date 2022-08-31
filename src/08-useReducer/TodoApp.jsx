c041b4856af3828b2d2e0e757454fe78654e1824
Mona Lisa <santifranquez@gmail.com>
2022-08-30 21:40:38 -0300

    Creando un custom hook para optimizar el codigo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,75 +1,26 @@
 import { useEffect } from "react"
 import { useReducer } from "react"
+import { useTodo } from "../hooks/useTodo"
 import { TodoAdd } from "./Components/TodoAdd"
 import { TodoList } from "./Components/TodoList"
 
 
-import { todoReducer } from "./todoReducer"
 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del alma',
-    //     done: false
-    // },
-    // {
-    //     id: new Date().getTime() + 100,
-    //     description: 'Recolectar la piedra del tiempo',
-    //     done: false
-    // },
-];
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos') || []); // Si es nulo regresa un arreglo vació
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    const  {
+        handleToggleTodo,
+        handleNewTodo,
+        handleDeleteTodo,
+        todos,
+        dispatch} = useTodo()
 
-    useEffect(() => {
-
-        // Traerlo a string
-        localStorage.setItem('todos', JSON.stringify(todos));
-      
-    }, [todos])
     
-
-
-    console.log(todos)
-
-    const handleNewTodo = (e) => {
-    
-        const action = {
-            // el type debe tener el mismo nombre que el case
-            type: '[TODO] Add Todo',
-            payload: e
-        };
-
-        // Le mandamos la acción al reducer
-        dispatch(action)
-    
-    };
-
-    const handleDeleteTodo = (id) => {
-        // console.log(id)
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    }
-
-    const handleToggleTodo = (id) => {
-        console.log(id)
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        });
-    }
 
     return (
         <>
-            <h1>TodoApp: {todos.length} <small>Pendientes: 2</small></h1>
+            <h1>TodoApp: {todos.length}</h1>
             <hr></hr>
 
             <div className="row">
---
+++ src/hooks/useTodo.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useReducer } from 'react';
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const initialState = [];
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos') || []); // Si es nulo regresa un arreglo vació
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    useEffect(() => {
+
+        // Traerlo a string
+        localStorage.setItem('todos', JSON.stringify(todos));
+      
+    }, [todos]);
+
+    const handleNewTodo = (e) => {
+    
+        const action = {
+            // el type debe tener el mismo nombre que el case
+            type: '[TODO] Add Todo',
+            payload: e
+        };
+
+        // Le mandamos la acción al reducer
+        dispatch(action)
+    
+    };
+
+    const handleDeleteTodo = (id) => {
+        // console.log(id)
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        console.log(id)
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+
+  return {
+        handleToggleTodo,
+        handleNewTodo,
+        handleDeleteTodo,
+        todos,
+        dispatch
+  }
+}
